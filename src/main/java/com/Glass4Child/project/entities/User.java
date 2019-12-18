db5992f46df0de5b295c1e9365901c78995473d2
LucasMR3 <lucas_mr@live.com>
2019-12-18 17:25:53 -0300

    User fix

--- src/main/java/com/Glass4Child/project/entities/User.java
+++ src/main/java/com/Glass4Child/project/entities/User.java
@@ -7,14 +7,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-
 @Entity
-public class User implements Serializable{
+public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long cpf;
+	private Long id;
 	private String name;
 	private String email;
 	private String address;
@@ -23,21 +22,21 @@ public class User implements Serializable{
 	public User() {
 	}
 
-	public User(Long cpf, String name, String email, String address, String password) {
+	public User(Long id, String name, String email, String address, String password) {
 		super();
-		this.cpf = cpf;
+		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.address = address;
 		this.password = password;
 	}
 
-	public Long getCpf() {
-		return cpf;
+	public Long getId() {
+		return id;
 	}
 
-	public void setCpf(Long cpf) {
-		this.cpf = cpf;
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public String getName() {
@@ -76,7 +75,7 @@ public class User implements Serializable{
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
 
@@ -89,14 +88,11 @@ public class User implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		if (cpf == null) {
-			if (other.cpf != null)
+		if (id == null) {
+			if (other.id != null)
 				return false;
-		} else if (!cpf.equals(other.cpf))
+		} else if (!id.equals(other.id))
 			return false;
 		return true;
 	}
-	
-	
-	
 }
